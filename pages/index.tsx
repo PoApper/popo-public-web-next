ba30a0607e05a62f38b820a655dacd50155a5dc1
bluehorn07 <bluehornblues@gmail.com>
2022-01-11 00:44:57 +0900

    polish index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
-import Layout from "../components/layout"
-import {Grid} from "semantic-ui-react";
+import Layout from '../components/layout'
+import { Grid } from 'semantic-ui-react'
 
 const HomePage = () => {
-    return (
-        <Layout>
-            <Grid columns="equal">
-                <Grid.Column>
-                    <h2>POPO, POstechian's POrtal</h2>
-                </Grid.Column>
-            </Grid>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <Grid columns="equal">
+        <Grid.Column>
+          <h2>POPO, POstechian&apos;s POrtal</h2>
+        </Grid.Column>
+      </Grid>
+    </Layout>
+  )
 }
 
 export default HomePage
