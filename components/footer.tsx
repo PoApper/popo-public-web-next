e38073d9bc2b65e92c5c3c6256ec0d0f2ba109e0
Seokyun Ha <bluehornblues@gmail.com>
2022-01-26 22:10:46 +0900

    implt footer (#6)

---
+++ components/footer.tsx
@@ -0,0 +1,90 @@
+import {
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Image,
+  List,
+  Segment,
+} from 'semantic-ui-react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <Segment vertical style={{
+        margin: '0em 0em',
+        padding: '2em 0em',
+        backgroundColor: 'none',
+      }}>
+        <Container textAlign="center">
+          <Grid divided stackable style={{ fontSize: 'small' }}>
+
+            <Grid.Column textAlign="left" width={7}>
+              <Header as="h3" content="POSTECH 총학생회 비상대책위원회"/>
+              <small>
+                <p>77 Cheongam-Ro. Nam-Gu. Pohang. Gyeongbuk. Korea 790-784</p>
+                <p>
+                  TEL +82-54-279-2621, 2637<br/>
+                  FAX +82-54-279-2626
+                </p>
+                <p>
+                  E-mail: stu-35@postech.ac.kr<br/>
+                  POPO 관련 문의: jeong314@postech.ac.kr
+                </p>
+              </small>
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <Header as="h4" content="Developed by"/>
+              <Image centered size={'small'}
+                     src={'/PoApper_logo.svg'} alt={'poapper-logo'}
+                     href={'https://club.poapper.com/'} target="_blank"/>
+              <List link>
+                <List.Item as="a" href="/recruiting">
+                  개발자 모집
+                </List.Item>
+              </List>
+              <List link>
+                <List.Item as="a" href="https://bit.ly/2XCtf3H">
+                  오류 신고 및 피드백
+                </List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <Header as="h4" content="POSTECH"/>
+              <List link>
+                <List.Item as="a" href="https://www.postech.ac.kr/"
+                           target="_blank">
+                  포항공대 홈페이지
+                </List.Item>
+                <List.Item as="a" href="https://povis.postech.ac.kr/"
+                           target="_blank">
+                  POVIS
+                </List.Item>
+                <List.Item as="a" href="https://hemos.postech.ac.kr/"
+                           target="_blank">
+                  HEMOS
+                </List.Item>
+                <List.Item as="a" href="https://library.postech.ac.kr/"
+                           target="_blank">
+                  박태준 학술정보관
+                </List.Item>
+              </List>
+            </Grid.Column>
+          </Grid>
+
+          <Divider section style={{ marginBottom: '1vh' }}/>
+          <List horizontal divided link size="small">
+            <List.Item as="a" href="#">
+              Contact Us
+            </List.Item>
+            <List.Item as="a" href="/privacy-policy">
+              Privacy Policy
+            </List.Item>
+          </List>
+        </Container>
+      </Segment>
+    </footer>
+  )
+}
+
+export default Footer
