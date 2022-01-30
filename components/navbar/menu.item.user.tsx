d8106263fc454d49375969d2e99e8063e4aab9f8
bluehorn07 <bluehornblues@gmail.com>
2022-01-31 00:59:48 +0900

    implt popo navbar

---
+++ components/navbar/menu.item.user.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Dropdown, Menu } from 'semantic-ui-react'
+import { useRouter } from 'next/router'
+
+const MenuItemUser = () => {
+  const router = useRouter()
+  const [user, setUser]: any = useState({})
+
+  useEffect(() => {
+    axios.get(
+      `${process.env.NEXT_PUBLIC_API}/auth/verifyToken`, {
+        withCredentials: true,
+      }).then((res) => setUser(res.data))
+      // .catch(() => router.push('/'))
+      .catch(() => console.log('TODO: 로그인 연동 이후 추가 개발'))
+  }, [])
+
+  const handleLogout = async () => {
+    try {
+      await axios.get(`${process.env.NEXT_PUBLIC_API}/auth/logout`, {
+        withCredentials: true,
+      })
+      await router.push('/login')
+    } catch (err) {
+      alert('로그아웃에 실패했습니다.')
+      console.log(err)
+    }
+  }
+
+  // TODO: refine this component, after login API sync
+  return (
+    <Menu.Item item simple position={'right'}>
+      <Dropdown text={'개발중!'}>
+        <Dropdown.Menu>
+          <Dropdown.Item text={'로그아웃'} onClick={handleLogout}/>
+        </Dropdown.Menu>
+      </Dropdown>
+    </Menu.Item>
+  )
+}
+
+export default MenuItemUser
--- components/navbar/navbar.desktop.tsx
+++ components/navbar/navbar.desktop.tsx
@@ -1,103 +1,124 @@
 import styled from 'styled-components'
-import { Button, Dropdown, Image, Menu } from 'semantic-ui-react'
-import Link from "next/link";
+import { Dropdown, Image, Menu } from 'semantic-ui-react'
+import Link from 'next/link'
+import MenuItemUser from './menu.item.user'
 
 const NavbarDesktop = () => {
-    return (
-        <NavbarMenu borderless>
-            <NavbarWrapper>
-                <NavbarInner>
-                    <Menu.Item>
-                        <Link href={'/reservation'}>
-                            <Dropdown item simple text={'장소/장비 예약'}>
-                                <NavbarDropdownMenu>
-                                    <Link href={'reservation/place'}>
-                                        <Dropdown.Item text={'장소 예약'}/>
-                                    </Link>
-                                    <Link href={'reservation/equip'}>
-                                        <Dropdown.Item text={'장비 예약'}/>
-                                    </Link>
-                                </NavbarDropdownMenu>
-                            </Dropdown>
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item>
-                        <Link href={'/association'}>
-                            <Dropdown item simple text={'총학생회'}>
-                                <NavbarDropdownMenu>
-                                    <Link href={'association/introduce'}>
-                                        <Dropdown.Item text={'자치단체 소개'}/>
-                                    </Link>
-                                    <Link href={'association/board'}>
-                                        <Dropdown.Item as={'a'} text={'총학생회 게시판'}/>
-                                    </Link>
-                                    <Dropdown.Item text={'총학생회 기록물관리기관'} target={'_blank'}
-                                                   href={'https://drive.google.com/drive/u/0/folders/1vHexwLSdD92maoKNlvw9zQ0q0J59k5FD'}/>
-                                </NavbarDropdownMenu>
-                            </Dropdown>
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item>
-                        <Link href={'/club'}>
-                            <Dropdown item simple text={'동아리'}>
-                                <NavbarDropdownMenu>
-                                    <Link href={'club/introduce'}>
-                                        <Dropdown.Item text={'동아리 소개'}/>
-                                    </Link>
-                                    <Link href={'club/board'}>
-                                        <Dropdown.Item as={'a'} text={'동아리 게시판'}/>
-                                    </Link>
-                                </NavbarDropdownMenu>
-                            </Dropdown>
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item>
-                        <Link href={'/whitebook'}>
-                            <Dropdown item simple text={'생활백서'}>
-                                <NavbarDropdownMenu>
-                                    <Dropdown.Item text={'생활관 자치회'}
-                                                   href={'https://dorm.postech.ac.kr/'}
-                                                   target="_blank"/>
-                                    <Dropdown.Item text={'배달업체'}
-                                                   href={'http://delivery.postech.ac.kr/'}
-                                                   target="_blank"/>
-                                </NavbarDropdownMenu>
-                            </Dropdown>
-                        </Link>
-                    </Menu.Item>
-                </NavbarInner>
-            </NavbarWrapper>
-        </NavbarMenu>
-    )
+  return (
+    <>
+      <NavbarNav>
+        <NavbarDiv>
+          <NavbarMenu borderless>
+
+            <Link href={'/'} passHref>
+              <Menu.Item style={{ paddingLeft: 0 }}>
+              <span style={{ textAlign: 'center' }}>
+                <Image
+                  centered
+                  src={'/popo.svg'} alt={'logo'}
+                  size={'small'}/>
+                <PopoFullText>
+                    Postechian&apos;s Portal
+                </PopoFullText>
+              </span>
+              </Menu.Item>
+            </Link>
+
+            <Dropdown item simple text="장소/장비 예약">
+              <Dropdown.Menu>
+                <Link href={'/reservation/place'} passHref>
+                  <Dropdown.Item as="a" text={'장소 예약'}/>
+                </Link>
+                <Link href={'/reservation/equip'} passHref>
+                  <Dropdown.Item as="a" text={'장비 예약'}/>
+                </Link>
+              </Dropdown.Menu>
+            </Dropdown>
+
+            <Dropdown item simple text="총학생회">
+              <Dropdown.Menu>
+                <Link href={'/association'} passHref>
+                  <Dropdown.Item as="a" text={'자치단체 소개'}/>
+                </Link>
+                <Dropdown.Item disabled as="a" text={'총학생회 게시판'}/>
+                <Dropdown.Item text={'총학생회 기록물관리기관'} target="_blank"
+                               href={'https://drive.google.com/drive/u/0/folders/1vHexwLSdD92maoKNlvw9zQ0q0J59k5FD'}/>
+              </Dropdown.Menu>
+            </Dropdown>
+
+            <Dropdown item simple text="동아리">
+              <Dropdown.Menu>
+                <Link href={'/club'} passHref>
+                  <Dropdown.Item as="a" text={'동아리 소개'}/>
+                </Link>
+                <Dropdown.Item disabled as="a" text={'동아리 게시판'}/>
+              </Dropdown.Menu>
+            </Dropdown>
+
+            <Dropdown item simple text="생활백서">
+              <Dropdown.Menu>
+                <Link href={'/whitebook'} passHref>
+                  <Dropdown.Item as="a" text={'생활백서'}/>
+                </Link>
+                <Dropdown.Item
+                  text={'생활관 자치회'}
+                  href={'https://dorm.postech.ac.kr/'} target={'_blank'}/>
+                <Dropdown.Item
+                  text={'배달업체'}
+                  href={'http://delivery.postech.ac.kr/'} target={'_blank'}/>
+              </Dropdown.Menu>
+            </Dropdown>
+
+            <MenuItemUser/>
+
+          </NavbarMenu>
+        </NavbarDiv>
+      </NavbarNav>
+    </>
+  )
 }
 
 export default NavbarDesktop
 
-const NavbarWrapper = styled.nav`
+const NavbarNav = styled.nav`
   border-bottom: 1px solid rgba(0, 0, 0, 0.1);
   background-color: white;
+
   font-weight: bold;
   width: 100%;
+
   position: fixed;
   top: 0;
   z-index: 10;
 `
 
-const NavbarInner = styled.div`
+const NavbarDiv = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   margin: auto;
-  max-width: ${({theme}) => theme.contentWidth};
+
+  max-width: ${({
+    theme,
+  }) => theme.contentWidth
+  };
 `
 
 const NavbarMenu = styled(Menu)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: auto;
+  gap: 1rem;
+
   box-shadow: none !important;
   border: none !important;
   width: 100%;
 `
 
-const NavbarDropdownMenu = styled(Dropdown.Menu)`
-  border: none;
-  boxShadow: 0 2px 5px 0px rgba(0, 0, 0, 0.2);
+const PopoFullText = styled.h1`
+  text-align: center;
+  margin-top: -0.4em;
+  font-family: 'Caveat', serif;
+  font-size: medium;
 `
