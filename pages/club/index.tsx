3b968eeee9cb7776a94ca1d385eac62f6801c65a
bluehorn07 <bluehornblues@gmail.com>
2022-02-04 14:12:52 +0900

    refactor club introduce page

--- pages/club/index.tsx
+++ pages/club/index.tsx
@@ -2,79 +2,35 @@ import Layout from '../../components/layout'
 import { Card, Grid, Image } from 'semantic-ui-react'
 
 const ClubIndexPage = () => {
+  const clubTypes = [
+    {slug: 'performance1', name: '공연1'},
+    {slug: 'performance2', name: '공연2'},
+    {slug: 'sports', name: '체육'},
+    {slug: 'hobbyAndExhibition', name: '취미전시'},
+    {slug: 'study', name: '학술'},
+    {slug: 'societyAndReligion', name: '사회종교'},
+  ]
+
   return (
     <Layout>
-      <Grid stackable style={{textAlign: "center"}} columns={3}>
-        <Grid.Row>
-          <Grid.Column>
-            <Card href={"/club/introduce/performance1"}>
-              <Card.Content style={{height: "12em"}}>
-                <Image src={'club/performance1.svg'} alt={'performance1'}
-                       style={{height: "100%", width: "100%", verticalAlign: "middle"}}/>
-              </Card.Content>
-              <Card.Content>
-                <Card.Header>공연1</Card.Header>
-              </Card.Content>
-            </Card>
-          </Grid.Column>
-          <Grid.Column>
-            <Card href={"/club/introduce/performance2"}>
-              <Card.Content style={{height: "12em"}}>
-                <Image src={'club/performance2.svg'} alt={'performance2'}
-                       style={{height: "100%", width: "100%", verticalAlign: "middle"}}/>
-              </Card.Content>
-              <Card.Content>
-                <Card.Header>공연2</Card.Header>
-              </Card.Content>
-            </Card>
-          </Grid.Column>
-          <Grid.Column>
-            <Card href={"/club/introduce/sports"}>
-              <Card.Content style={{height: "12em"}}>
-                <Image src={'club/sports.svg'} alt={'sports'}
-                       style={{height: "100%", width: "100%", verticalAlign: "middle"}}/>
-              </Card.Content>
-              <Card.Content>
-                <Card.Header>체육</Card.Header>
-              </Card.Content>
-            </Card>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column>
-            <Card href={"/club/introduce/hobbyAndExhibition"}>
-              <Card.Content style={{height: "12em"}}>
-                <Image src={'club/hobbyAndExhibition.svg'} alt={'hobbyAndExhibition'}
-                       style={{height: "100%", width: "100%", verticalAlign: "middle"}}/>
-              </Card.Content>
-              <Card.Content>
-                <Card.Header>취미전시</Card.Header>
-              </Card.Content>
-            </Card>
-          </Grid.Column>
-          <Grid.Column>
-            <Card href={"/club/introduce/study"}>
-              <Card.Content style={{height: "12em"}}>
-                <Image src={'club/study.svg'} alt={'study'}
-                       style={{height: "100%", width: "100%", verticalAlign: "middle"}}/>
-              </Card.Content>
-              <Card.Content>
-                <Card.Header>학술</Card.Header>
-              </Card.Content>
-            </Card>
-          </Grid.Column>
-          <Grid.Column>
-            <Card href={"/club/introduce/societyAndReligion"}>
-              <Card.Content style={{height: "12em"}}>
-                <Image src={'club/societyAndReligion.svg'} alt={'societyAndReligion'}
-                       style={{height: "100%", width: "100%", verticalAlign: "middle"}}/>
-              </Card.Content>
-              <Card.Content>
-                <Card.Header>사회종교</Card.Header>
-              </Card.Content>
-            </Card>
-          </Grid.Column>
-        </Grid.Row>
+      <Grid stackable style={{textAlign: "center"}} rows={2} columns={3}>
+        {
+          clubTypes.map(({slug, name}) => {
+            return (
+              <Grid.Column key={slug}>
+                <Card href={`/club/introduce/${slug}`}>
+                  <Card.Content style={{height: "12em"}}>
+                    <Image src={`club/${slug}.svg`} alt={slug}
+                           style={{height: "100%", width: "100%", verticalAlign: "middle"}}/>
+                  </Card.Content>
+                  <Card.Content>
+                    <Card.Header>{name}</Card.Header>
+                  </Card.Content>
+                </Card>
+              </Grid.Column>
+            )
+          })
+        }
       </Grid>
     </Layout>
   )
