f63a5415bd2bac6a0310b38cfb5787e1834c9c86
bluehorn07 <bluehornblues@gmail.com>
2022-02-11 01:50:14 +0900

    separate usage text to make pure component

--- components/reservation/reservation.calendar.tsx
+++ components/reservation/reservation.calendar.tsx
@@ -1,52 +1,44 @@
-import {useRouter} from "next/router";
-import {useEffect, useState} from "react";
-import axios from "axios";
-import {DateInput} from "semantic-ui-calendar-react";
-import {Label} from "semantic-ui-react";
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { DateInput } from 'semantic-ui-calendar-react'
 
 type reservationType = {
-    date: string
+  date: string
 }
 
-const ReservationCalendar: React.FunctionComponent<{ selectedDate: string }> = (props) => {
-    const router = useRouter();
-    const selectedDate = router.query.selectedDate as string;
-    const owner = router.query.owner;
-    const [reservations, setReservations] = useState<reservationType[]>([]);
+const ReservationCalendar = () => {
+  const router = useRouter()
+  const selectedDate = router.query.selectedDate as string
+  const owner = router.query.owner
+  const [reservations, setReservations] = useState<reservationType[]>([])
 
-    useEffect(() => {
-        axios.get( `${process.env.NEXT_PUBLIC_API}/reservation-equip?owner=${owner}`).then((res) => {
-            setReservations(res.data);
-        })
-    }, [selectedDate, owner])
+  useEffect(() => {
+    axios.get(
+      `${process.env.NEXT_PUBLIC_API}/reservation-equip?owner=${owner}`).
+      then((res) => {
+        setReservations(res.data)
+      })
+  }, [selectedDate, owner])
 
-    const markedDates: Date[] = [];
-    for (const reservation of reservations) {
-        const date = reservation.date;
-    }
+  const markedDates: Date[] = []
+  for (const reservation of reservations) {
+    const date = reservation.date
+  }
 
-    return (
-        <>
-            <DateInput
-                inline
-                name={"date"}
-                markColor={"orange"}
-                value = {selectedDate}
-                marked={markedDates}
-                dateFormat={"YYYYMMDD"}
-                onChange={(e, data) => {
-                    e.preventDefault()
-                }}
-            />
-            <p>날짜를 고르면, 예약 현황을 확인할 수 있습니다! 😎</p>
-            <p>해당 날짜에 예약이 하나라도 존재하면, 달력에 <Label circular color={"orange"} empty/>로 표시됩니다.</p>
-            <p>
-                <b>심사중</b>은 <Label circular color={"black"} empty/> 로, &nbsp;
-                <b>통과</b>는 <Label circular color={"green"} empty/> 로, &nbsp;
-                <b>거절</b>은 <Label circular color={"red"} empty/> 로 표시됩니다.
-            </p>
-        </>
-    );
+  return (
+    <DateInput
+      inline
+      name={'date'}
+      markColor={'orange'}
+      value={selectedDate}
+      marked={markedDates}
+      dateFormat={'YYYYMMDD'}
+      onChange={(e, data) => {
+        e.preventDefault()
+      }}
+    />
+  )
 }
 
 export default ReservationCalendar
--- pages/reservation/place/[region]/[placeName].tsx
+++ pages/reservation/place/[region]/[placeName].tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Grid } from 'semantic-ui-react'
+import { Grid, Label } from 'semantic-ui-react'
 import { useRouter } from 'next/router'
 import axios from 'axios'
 import moment from 'moment'
@@ -9,7 +9,7 @@ import PlaceReservationTable
   from '../../../../components/reservation/place.reservation.table'
 import ReservationCalendar
   from '../../../../components/reservation/reservation.calendar'
-import PlaceInfoCard from '../../../../components/reservation/placeInfoCard'
+import PlaceInformationCard from '../../../../components/reservation/place.information.card'
 
 type bookerType = {
   name: string,
@@ -17,6 +17,7 @@ type bookerType = {
 }
 
 type reservationType = {
+  uuid: string,
   booker: bookerType,
   date: string,
   description: string,
@@ -53,18 +54,36 @@ const RegionPlace: React.FunctionComponent = () => {
       <Grid columns={2} divided stackable>
 
         <Grid.Column width={6}>
-          <PlaceInfoCard placeName={placeName}/>
+          <PlaceInformationCard placeName={placeName}/>
         </Grid.Column>
 
         <Grid.Column>
           <Grid rows={2} divided stackable>
             <Grid.Column>
+
               <Grid.Row centered style={{ marginBottom: '1em' }}>
                 <ReservationCalendar selectedDate={selectedDate}/>
               </Grid.Row>
+
+              <Grid.Row style={{ marginBottom: '1em' }}>
+                <p>
+                  날짜를 고르면, 예약 현황을 확인할 수 있습니다! 😎
+                </p>
+                <p>
+                  해당 날짜에 예약이 하나라도 존재하면,
+                  달력에 <Label circular color={'orange'} empty/>로 표시됩니다.
+                </p>
+                <p>
+                  <b>심사중</b>은 <Label circular color={'black'} empty/> 로, &nbsp;
+                  <b>통과</b>는 <Label circular color={'green'} empty/> 로, &nbsp;
+                  <b>거절</b>은 <Label circular color={'red'} empty/> 로 표시됩니다.
+                </p>
+              </Grid.Row>
+
               <Grid.Row>
                 <PlaceReservationTable reservations={reservations}/>
               </Grid.Row>
+
             </Grid.Column>
           </Grid>
         </Grid.Column>
