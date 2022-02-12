05b2407a515799077c1eabbeeffcac34185bc18e
bluehorn07 <bluehornblues@gmail.com>
2022-02-13 00:00:23 +0900

    refactor place/equipment index page

--- pages/reservation/equipment/index.tsx
+++ pages/reservation/equipment/index.tsx
@@ -2,14 +2,14 @@ import { Card, Grid, Image } from 'semantic-ui-react'
 import styled from 'styled-components'
 import Layout from '../../../components/layout'
 
-const EquipmentPage = () => {
+const EquipmentIndexPage = () => {
   return (
     <Layout>
       <div>
         <Grid stackable columns={3} centered>
           <Grid.Column>
-            <Card href={'/reservation/equip/dongyeon'} centered>
-              <LogoImage src={'/equip/dongyeon.png'} alt={'dongyeon'}/>
+            <Card href={'/reservation/equipment/dongyeon'} centered>
+              <LogoImage src={'/reservation/dongyeon.png'} alt={'dongyeon'}/>
               <Card.Content>
                 <Card.Header>동아리연합회</Card.Header>
                 <Card.Meta>Student Club Union</Card.Meta>
@@ -17,8 +17,8 @@ const EquipmentPage = () => {
             </Card>
           </Grid.Column>
           <Grid.Column>
-            <Card href={'/reservation/equip/dorm-union'} centered>
-              <LogoImage src={'/equip/dormUnion.png'} alt={'dormUnion'}/>
+            <Card href={'/reservation/equipment/dorm-union'} centered>
+              <LogoImage src={'/reservation/dormUnion.png'} alt={'dormUnion'}/>
               <Card.Content>
                 <Card.Header>생활관자치회</Card.Header>
                 <Card.Meta>Dormitory Union</Card.Meta>
@@ -26,8 +26,8 @@ const EquipmentPage = () => {
             </Card>
           </Grid.Column>
           <Grid.Column>
-            <Card href={'reservation/equip/saengna'} centered>
-              <LogoImage src={'/equip/saengna.jpg'} alt={'saengna'}/>
+            <Card href={'reservation/equipment/saengna'} centered>
+              <LogoImage src={'/reservation/saengna.jpg'} alt={'saengna'}/>
               <Card.Content>
                 <Card.Header>생각나눔</Card.Header>
                 <Card.Meta>Committee Advisory Council</Card.Meta>
@@ -40,7 +40,7 @@ const EquipmentPage = () => {
   )
 }
 
-export default EquipmentPage
+export default EquipmentIndexPage
 
 const LogoImage = styled(Image)`
   background-color: white !important;
--- pages/reservation/place/index.tsx
+++ pages/reservation/place/index.tsx
@@ -1,44 +1,40 @@
-import {Card, Grid, Image} from "semantic-ui-react";
-import Layout from "../../../components/layout";
+import { Card, Grid, Image } from 'semantic-ui-react'
+import Layout from '../../../components/layout'
 
-const PlacePage = () => {
-    return (
-        <Layout>
-            <div>
-                <Grid>
-                    <Grid.Row centered columns={3}>
-                        <Grid.Column>
-                            <Card href={'place/student-hall'} centered>
-                                <Image src={"/place/student_hall.jpg"}/>
-                                <Card.Content>
-                                    <Card.Header>학생회관</Card.Header>
-                                    <Card.Meta>Student Hall</Card.Meta>
-                                </Card.Content>
-                            </Card>
-                        </Grid.Column>
-                        <Grid.Column>
-                            <Card href={'place/jigok'} centered>
-                                <Image src={"/place/jigok.jpg"}/>
-                                <Card.Content>
-                                    <Card.Header>지곡회관</Card.Header>
-                                    <Card.Meta>Ji-gok Community Center</Card.Meta>
-                                </Card.Content>
-                            </Card>
-                        </Grid.Column>
-                        <Grid.Column>
-                            <Card href={'place/others'} centered>
-                                <Image src={"/place/dormitory_img.jpg"}/>
-                                <Card.Content>
-                                    <Card.Header>생활관 외</Card.Header>
-                                    <Card.Meta>Dormitory, etc.</Card.Meta>
-                                </Card.Content>
-                            </Card>
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-            </div>
-        </Layout>
-    )
+const PlaceIndexPage = () => {
+  return (
+    <Layout>
+      <Grid stackable centered columns={3}>
+        <Grid.Column>
+          <Card href={'/reservation/place/student-hall'} centered>
+            <Image src={'/reservation/student_hall.jpg'} alt={'student_hall'}/>
+            <Card.Content>
+              <Card.Header>학생회관</Card.Header>
+              <Card.Meta>Student Hall</Card.Meta>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+        <Grid.Column>
+          <Card href={'/reservation/place/jigok'} centered>
+            <Image src={'/reservation/jigok.jpg'} alt={'jigok'}/>
+            <Card.Content>
+              <Card.Header>지곡회관</Card.Header>
+              <Card.Meta>Ji-gok Community Center</Card.Meta>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+        <Grid.Column>
+          <Card href={'/reservation/place/others'} centered>
+            <Image src={'/reservation/dormitory.jpg'} alt={'others'}/>
+            <Card.Content>
+              <Card.Header>생활관 외</Card.Header>
+              <Card.Meta>Dormitory, etc.</Card.Meta>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+      </Grid>
+    </Layout>
+  )
 }
 
-export default PlacePage
+export default PlaceIndexPage
