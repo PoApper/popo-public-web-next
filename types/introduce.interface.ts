19060f5fb2161d7079e9f1356e3f95bfbfafc214
bluehorn07 <bluehornblues@gmail.com>
2022-02-15 23:09:04 +0900

    implt introduce interface

---
+++ types/introduce.interface.ts
@@ -0,0 +1,21 @@
+/**
+ * <Quick Rule>
+ * For `React Component Props`, use `type`
+ * For other object-like or class-like stuffs, use `interface`
+ */
+
+export interface IClubIntroduce {
+  content: string;
+  location: string;
+  representative: string;
+  contact: string;
+  logoName: string;
+}
+
+export interface IAssociationIntroduce {
+  content: string;
+  location: string;
+  representative: string;
+  contact: string;
+  logoName: string;
+}
