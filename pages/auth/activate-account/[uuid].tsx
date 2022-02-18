b3b28b27b683eb1a53872463b54abfdcf2c60c21
bluehorn07 <bluehornblues@gmail.com>
2022-02-18 22:21:04 +0900

    add account activate page

---
+++ pages/auth/activate-account/[uuid].tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import axios from 'axios'
+import { Button } from 'semantic-ui-react'
+
+const ActivateAccountPage = () => {
+  const router = useRouter()
+  const userUuid = router.query.uuid
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [isValidAccount, setIsValidAccount] = useState(false)
+
+  useEffect(() => {
+    if (!userUuid) return;
+
+    setIsLoading(true)
+    axios.put(`${process.env.NEXT_PUBLIC_API}/auth/activate/${userUuid}`,
+      { withCredentials: true }).then(
+      () => {
+        setIsLoading(false)
+        setIsValidAccount(true)
+      }).catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+        setIsValidAccount(false);
+        alert('올바르지 않은 접근입니다.');
+    })
+  }, [userUuid])
+
+  return (
+    <div>
+      {
+        isLoading ? (
+          <div>
+            계정 정보를 확인하고 있습니다...
+          </div>
+        ) : (
+          isValidAccount ? (
+            <div>
+              <h2>계정이 활성 되었습니다!</h2>
+              <p>
+                POSTECH 총학생회에서 운영하는 POPO는 학생 복지와 편의를 제공합니다.<br/>
+                POPO에서 여러 기능들을 사용해보세요!
+              </p>
+              <br/>
+              <Button primary href={'/auth/login'}>
+                로그인 하러 가기
+              </Button>
+            </div>
+          ) : (
+            <div>
+              <h2>올바르지 않는 접근입니다.</h2>
+            </div>
+          )
+        )
+      }
+    </div>
+  )
+}
+
+export default ActivateAccountPage
