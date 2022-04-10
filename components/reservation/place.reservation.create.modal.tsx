2b9fc9bab2b9bdb222fd95d6daa7acde209ffbba
jjeongone <jjeongone@gmail.com>
2022-04-10 19:28:46 +0900

    add community center to reservation page

--- components/reservation/place.reservation.create.modal.tsx
+++ components/reservation/place.reservation.create.modal.tsx
@@ -16,6 +16,7 @@ const RegionKorNameMapping = {
   STUDENT_HALL: '학생 회관',
   JIGOK_CENTER: '지곡 회관',
   OTHERS: '생활관 외',
+  COMMUNITY_CENTER : '커뮤니티 센터',
 }
 
 type PlaceReservationCreateModalProps = {
