8a328861a0b8b1084aeab55c4b2613db6d747ec2
bluehorn07 <bluehornblues@gmail.com>
2022-10-03 20:46:19 +0600

    add return type

--- lib/time-date.ts
+++ lib/time-date.ts
@@ -18,7 +18,10 @@ export function hourDiff(startTime: string, endTime: string): number {
   return duration.asHours();
 }
 
-export function roundUpByDuration(date: moment.Moment, durationMinutes: number = 30) {
+export function roundUpByDuration(
+  date: moment.Moment,
+  durationMinutes: number = 30
+): moment.Moment {
   const remainder = durationMinutes - (date.minute() % durationMinutes);
   return moment(date).add(remainder, "minutes").second(0)
 }
