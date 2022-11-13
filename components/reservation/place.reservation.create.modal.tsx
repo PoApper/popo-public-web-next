6ebb1eed768d3a115a720fbbfbfa0f24d93313ee
bluehorn07 <bluehornblues@gmail.com>
2022-11-13 22:01:22 +0900

    make component for reservation datetime picker

--- components/reservation/place.reservation.create.modal.tsx
+++ components/reservation/place.reservation.create.modal.tsx
@@ -1,18 +1,15 @@
-import React, { KeyboardEvent, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { Button, Divider, Form, Message, Modal } from 'semantic-ui-react'
 import axios from 'axios'
 import moment from 'moment'
-
-import { DateInput } from 'semantic-ui-calendar-react'
-import DatePicker from 'react-datepicker'
-import 'react-datepicker/dist/react-datepicker.css'
 
 import { IUser } from '../../types/user.interface'
 import { IPlace } from '../../types/reservation.interface'
 import { hourDiff, roundUpByDuration } from '../../lib/time-date'
 import OpeningHoursList from './opening_hours.list'
 import { isOnOpeningHours } from '../../lib/opening_hours'
+import ReservationDatetimePicker from './reservation.datetime.picker'
 
 const RegionKorNameMapping = {
   STUDENT_HALL: '학생 회관',
@@ -146,61 +143,10 @@ const PlaceReservationCreateModal
           <Divider/>
 
           <Form.Group>
-            <div className={'required field'}>
-              <label>날짜</label>
-              <DateInput
-                dateFormat={'yyyy-MM-DD'}
-                minDate={moment()} maxDate={moment().add(30, 'day')}
-                value={date.format('YYYY-MM-DD')}
-                onKeyDown={(e: KeyboardEvent) => e.preventDefault()}
-                onChange={(_, value) => {
-                  const targetDate: string = value.value // YYYY-MM-DD
-                  if (targetDate === now.format('YYYY-MM-DD')) {
-                    setDate(now);
-                    setStartTime(now);
-                    setEndTime(nowNext30Min);
-                  } else {
-                    setDate(moment(targetDate + 'T00:00'));
-                    setStartTime(moment(targetDate + 'T00:00'));
-                    setEndTime(moment(targetDate + 'T00:30'));
-                  }
-                }}/>
-            </div>
-
-            <div className={'required field'}>
-              <label>시작 시간</label>
-              <DatePicker
-                showTimeSelect showTimeSelectOnly timeIntervals={30}
-                onKeyDown={e => e.preventDefault()}
-                dateFormat={'hh:mm aa'}
-                selected={startTime.toDate()}
-                minTime={date.toDate()}
-                maxTime={moment(date.format('YYYY-MM-DD') + 'T23:59').toDate()}
-                onChange={(startTime: Date) => {
-                  const newStartTime = moment(startTime);
-                  const newStartTimeNext30Min = moment(newStartTime).add(30, 'minute');
-                  setStartTime(newStartTime);
-                  setEndTime(newStartTimeNext30Min);
-                }}/>
-            </div>
-
-            <div className={'required field'}>
-              <label>종료 시간</label>
-              <DatePicker
-                showTimeSelect showTimeSelectOnly timeIntervals={30}
-                onKeyDown={e => e.preventDefault()}
-                dateFormat={'hh:mm aa'}
-                selected={endTime.toDate()}
-                minTime={
-                  moment(startTime).add(30, 'minute').toDate()
-                }
-                maxTime={
-                  (endTime.format('HHmm') === '0000') ?
-                    moment(date.format('YYYY-MM-DD') + 'T00:00').toDate() // edge-case
-                    : moment(date.format('YYYY-MM-DD') + 'T23:59').toDate()
-                }
-                onChange={(endTime: Date) => {setEndTime(moment(endTime))}}/>
-            </div>
+            <ReservationDatetimePicker
+              date={date} startTime={startTime} endTime={endTime}
+              setDate={setDate} setStartTime={setStartTime} setEndTime={setEndTime}
+            />
           </Form.Group>
 
           {
