b3a832fe50480815f1e71adfdc06233bcbef145e
bluehorn07 <bluehornblues@gmail.com>
2022-11-25 00:00:49 +0900

    mobile navbar

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,15 +1,20 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useState } from 'react'
 import Head from 'next/head'
 import styled, { ThemeProvider } from 'styled-components'
 import theme from '../styles/theme'
 import NavbarDesktop from './navbar/navbar.desktop'
 import Footer from './footer'
+import { Desktop, Mobile } from './media-query'
+import NavbarMobile from './navbar/navbar.mobile'
+import SideBar from './navbar/sidebar'
 
 type LayoutProps = {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const [sidebarVisible, setSidebarVisible] = useState(false)
+
   return (
     <ThemeProvider theme={theme}>
       <Head>
@@ -18,12 +23,32 @@ const Layout = ({ children }: LayoutProps) => {
         <link rel="icon" href={'/favicon.ico'}/>
       </Head>
       <main>
-        <NavbarDesktop/>
-        <Wrapper>
-          <div style={{ width: '100%' }}>
-            { children }
-          </div>
-        </Wrapper>
+        <Mobile criteria={"768px"}>
+          <NavbarMobile
+            openSidebar={() => setSidebarVisible(true)}
+          />
+          <SideBar
+            visible={sidebarVisible}
+            toggleSidebar={() => setSidebarVisible(!sidebarVisible)}
+            pushContent={
+              <Wrapper>
+                <div style={{ width: '100%' }}>
+                  {children}
+                </div>
+              </Wrapper>
+            }
+          />
+        </Mobile>
+
+        <Desktop criteria={"768px"}>
+          <NavbarDesktop/>
+          <Wrapper>
+            <div style={{ width: '100%' }}>
+              { children }
+            </div>
+          </Wrapper>
+        </Desktop>
+
         <Footer/>
       </main>
     </ThemeProvider>
---
+++ components/media-query.tsx
@@ -0,0 +1,36 @@
+import React, {ReactNode} from 'react'
+import styled from "styled-components";
+
+export const Mobile  = ({ children, criteria }: {
+  children: ReactNode,
+  criteria: string // 480px
+}) => {
+  return (
+    <MobileWrapper criteria={criteria}>
+      {children}
+    </MobileWrapper>
+  )
+}
+
+const MobileWrapper = styled.div<{criteria: string}>`
+  @media only screen and (min-width: ${props => props.criteria}) {
+    display: none !important;
+  }
+`
+
+export const Desktop  = ({ children, criteria }: {
+  children: ReactNode
+  criteria: string // 960px
+}) => {
+  return (
+    <DesktopWrapper criteria={criteria}>
+      {children}
+    </DesktopWrapper>
+  )
+}
+
+const DesktopWrapper = styled.div<{criteria: string}>`
+  @media only screen and (max-width: ${props => props.criteria}) {
+    display: none !important;
+  }
+`
