faaad6871a2c17491100570a8dfeaac86b0d6d04
bluehorn07 <bluehornblues@gmail.com>
2023-02-12 21:19:19 +0900

    simpler script

--- ecr-script.sh
+++ ecr-script.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env bash
+ECR_REPOSITORY=151345152001.dkr.ecr.ap-northeast-2.amazonaws.com
 aws ecr get-login-password --region ap-northeast-2 | \
-  docker login --username AWS --password-stdin 151345152001.dkr.ecr.ap-northeast-2.amazonaws.com
+  docker login --username AWS --password-stdin ${ECR_REPOSITORY}
 docker build . -t popo-public-web
-docker image tag popo-public-web:latest 151345152001.dkr.ecr.ap-northeast-2.amazonaws.com/popo-public-web:latest
-docker push 151345152001.dkr.ecr.ap-northeast-2.amazonaws.com/popo-public-web:latest
+docker image tag popo-public-web:latest ${ECR_REPOSITORY}/popo-public-web:latest
+docker push ${ECR_REPOSITORY}/popo-public-web:latest
