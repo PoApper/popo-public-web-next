7811102290b959c71e2b6c744a90094e0ef31012
bluehorn07 <bluehornblues@gmail.com>
2023-05-09 23:24:40 +0900

    restore react-calendar css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import '../styles/globals.css'
-import 'semantic-ui-css/semantic.min.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import 'semantic-ui-css/semantic.min.css';
+import 'react-calendar/dist/Calendar.css';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
