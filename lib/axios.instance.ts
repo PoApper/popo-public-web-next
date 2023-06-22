3364a4458b5924cc3302fa9d3efa43f83ff4d83d
bluehorn07 <bluehornblues@gmail.com>
2023-06-23 00:27:17 +0900

    update dev url

--- lib/axios.instance.ts
+++ lib/axios.instance.ts
@@ -5,7 +5,7 @@ const next_env = process.env.NEXT_PUBLIC_ENV;
 export let popoApiUrl = (
   next_env === "prod" ?
     "https://api.popo.poapper.club" :
-    "https://api.dev.popo.poapper.club"
+    "https://api.popo-dev.poapper.club"
 );
 
 export const PoPoAxios = axios.create({
