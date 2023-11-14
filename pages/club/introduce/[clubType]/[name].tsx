2b982cb2a3756b3ccd9d09e110f5e5939a288e4f
bluehorn07 <bluehornblues@gmail.com>
2023-11-15 00:10:59 +0900

    refactor club intro page

--- pages/club/introduce/[clubType]/[name].tsx
+++ pages/club/introduce/[clubType]/[name].tsx
@@ -10,60 +10,58 @@ import { PoPoAxios } from '@/lib/axios.instance'
 const ClubSingleIntroducePage: React.FunctionComponent<{
   name: string,
   clubInfo: IClubIntroduce,
-}> = ({ name, clubInfo }) => {
+  recommendations: IClubIntroduce[],
+}> = ({ name, clubInfo, recommendations }) => {
   return (
     <Layout>
-      <Grid>
-        <Grid.Row columns={2}>
-          <Grid.Column width={12}>
-            <h1 style={{margin: 0}}>
-              {name}
-            </h1>
-            <h3 style={{margin: "0 0 10px", color: 'gray'}}>{clubInfo.short_desc}</h3>
+      <div style={{padding: 8}}>
+        <div style={{marginBottom: 4}}>
+          <Image
+            size='small'
+            src={clubInfo.image_url ?? 'https://react.semantic-ui.com/images/wireframe/image.png'}
+            alt={`${name}_logo`}
+          />
+        </div>
 
-            <Container style={{fontSize: 18, display: 'inline-flex', gap: 2}}>
-              <IconLink link={clubInfo.homepage_url}>
-                <img src={'https://img.shields.io/badge/website-000000?style=for-the-badge'}
-                     alt={'homepage'}/>
-              </IconLink>
-              <IconLink link={clubInfo.facebook_url}>
-                <img src={'https://img.shields.io/badge/Facebook-1877F2?style=for-the-badge&logo=facebook&logoColor=white'}
-                     alt={'facebook'}/>
-              </IconLink>
-              <IconLink link={clubInfo.instagram_url}>
-                <img src={'https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white'}
-                     alt={'instagram'}/>
-              </IconLink>
-              <IconLink link={clubInfo.youtube_url}>
-                <img src={'https://img.shields.io/badge/Youtube-FF0000?style=for-the-badge&logo=youtube&logoColor=white'}
-                     alt={'youtube'}/>
-              </IconLink>
-            </Container>
+        <h1 style={{margin: "0 auto"}}>
+          {name}
+        </h1>
+        <h2 style={{color: "grey", marginTop: 0}}>
+          {clubInfo.short_desc}
+        </h2>
 
-            <Container style={{fontSize: 16}}>
-              {clubInfo.content}
-            </Container>
+        <div style={{fontSize: 18}}>
+          <IconLink link={clubInfo.homepage_url}>
+            <img src={'https://img.shields.io/badge/website-000000?style=for-the-badge'}
+                  alt={'homepage'}/>
+          </IconLink>
+          <IconLink link={clubInfo.facebook_url}>
+            <img src={'https://img.shields.io/badge/Facebook-1877F2?style=for-the-badge&logo=facebook&logoColor=white'}
+                  alt={'facebook'}/>
+          </IconLink>
+          <IconLink link={clubInfo.instagram_url}>
+            <img src={'https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white'}
+                  alt={'instagram'}/>
+          </IconLink>
+          <IconLink link={clubInfo.youtube_url}>
+            <img src={'https://img.shields.io/badge/Youtube-FF0000?style=for-the-badge&logo=youtube&logoColor=white'}
+                  alt={'youtube'}/>
+          </IconLink>
+        </div>
 
-            <br/>
+        <div style={{fontSize: 16, margin: "12px 0"}}>
+          {clubInfo.content}
+        </div>
 
-            <Container>
-              <p>
-                <b>동방 위치</b>: {clubInfo.location}
-              </p>
-              <p>
-                <b>대표자</b>: {clubInfo.representative}({clubInfo.contact})
-              </p>
-            </Container>
-          </Grid.Column>
-          <Grid.Column width={4}>
-            <Image
-              centered size='small'
-              src={clubInfo.image_url ?? 'https://react.semantic-ui.com/images/wireframe/image.png'}
-              alt={`${name}_logo`}
-            />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+        <div>
+          <p>
+            <b>동방 위치</b>: {clubInfo.location}
+          </p>
+          <p>
+            <b>대표자</b>: {clubInfo.representative}({clubInfo.contact})
+          </p>
+        </div>
+      </div>
     </Layout>
   )
 }
@@ -73,10 +71,14 @@ export default ClubSingleIntroducePage;
 export const getServerSideProps: GetServerSideProps  = async (context) => {
   const name = context.query['name'] as string;
 
-  const res = await PoPoAxios.get<IClubIntroduce>(`introduce/club/name/${name}`);
-  const clubInfo = res.data;
+  const res1 = await PoPoAxios.get<IClubIntroduce>(`introduce/club/name/${name}`);
+  const clubInfo = res1.data;
+
+  // const res2 = await PoPoAxios.get<IClubIntroduce[]>(`introduce/club/recommendation/${clubInfo.uuid}`);
+  const res2 = await PoPoAxios.get<IClubIntroduce[]>(`introduce/club/`);
+  const recommendations = res2.data;
 
   return {
-    props: { name, clubInfo }
+    props: { name, clubInfo, recommendations }
   };
 };
