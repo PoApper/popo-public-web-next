a3c59c17bf1d03340e29eb901b327f75b736b690
Seokyun Ha <bluehornblues@gmail.com>
2023-12-11 16:33:55 +0900

    feat: Notice (#77)

---
+++ components/notice/notice.panel.tsx
@@ -0,0 +1,43 @@
+import { INotice } from '@/types/notice.interface'
+import { Image } from 'semantic-ui-react';
+import styled from 'styled-components'
+
+const NoticePanel = ({
+   noticeList 
+}: { noticeList: INotice[] }) => {
+  return (
+    <div style={{flex: 1, display: 'flex', flexDirection: 'column', gap: 8 }}>
+      { 
+        noticeList.map((notice) => 
+        <NoticeCard key={notice.id}>
+          <div style={{fontWeight: 700, fontSize: 18, textDecoration: 'none'}}>
+            {
+              notice.link ? (
+                <a href={notice.link} target={'_blank'} style={{color: 'black'}}>
+                  {notice.title} 🔗
+                </a>
+              ) : notice.title
+            }
+          </div>
+          <div style={{marginTop: 8, whiteSpace: "pre-line", textAlign: "left"}}>
+            {notice.content}
+          </div>
+          {
+            notice.image_url ? (
+              <Image src={notice.image_url}/>
+            ) : null
+          }
+        </NoticeCard>
+      )
+      }
+    </div>
+  )
+}
+
+export default NoticePanel;
+
+const NoticeCard = styled.div`
+  background: #eeeeee;
+  border-radius: 0.4em;
+  padding: 14px;
+`
