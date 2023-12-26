ab1c00b26d6965d749860bb359bc97280210492a
bluehorn07 <bluehornblues@gmail.com>
2023-12-26 20:50:49 +0900

    display max concurrent place reservation

--- components/reservation/place.information.card.tsx
+++ components/reservation/place.information.card.tsx
@@ -20,6 +20,22 @@ const PlaceInformationCard: FunctionComponent<{
         <Card.Description>{placeInfo.description}</Card.Description>
         <Card.Meta style={{marginTop: 8}}>
           <OpeningHoursList openingHours={JSON.parse(placeInfo.opening_hours)}/>
+          {
+            (placeInfo.max_minutes !== 24 * 60 || placeInfo.max_concurrent_reservation > 1) ? (
+              <ul style={{paddingLeft: 16}}>
+                {
+                  placeInfo.max_minutes !== 24 * 60 ? (
+                    <li>최대 예약 기간: {placeInfo.max_minutes}분</li>
+                  ) : null
+                }
+                {
+                  placeInfo.max_concurrent_reservation > 1 ? (
+                    <li>동시 예약 가능 갯수: 최대 {placeInfo.max_concurrent_reservation}개 예약</li>
+                  ) : null
+                }
+              </ul>
+            ) : null
+          }
         </Card.Meta>
       </Card.Content>
     </Card>
