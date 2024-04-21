f16210608cec8e8b0bd43c64840f869ca7773bc2
Gwanho Kim <khkim6040@postech.ac.kr>
2024-04-21 22:08:39 +0900

    Configure format and linting setting
    Same setting as popo-admin-web repo

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@next/next/recommended',
+    'next/core-web-vitals',
+  ],
+  rules: {
+    // 'react/prop-types': 'off',
+    // 'react/react-in-jsx-scope': 'off',
+  },
+};
