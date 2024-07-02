3fdb685c94aacb76065f3eb56d8f7b0a163ef41e
Gwanho Kim <khkim6040@postech.ac.kr>
2024-07-02 21:42:10 +0900

    Make state

--- components/notice/notice.panel.tsx
+++ components/notice/notice.panel.tsx
@@ -1,8 +1,21 @@
 import { INotice } from '@/types/notice.interface';
+import { useState } from 'react';
 import { Image } from 'semantic-ui-react';
 import styled from 'styled-components';
 
 const NoticePanel = ({ noticeList }: { noticeList: INotice[] }) => {
+  const [isLike, setIsLike] = useState<boolean[]>(
+    new Array(noticeList.length).fill(false),
+  );
+  const handleLike = (id: number) => {
+    setIsLike((prevIsLike) => {
+      const newLike = [...prevIsLike];
+      newLike[id] = !newLike[id];
+      return newLike;
+    });
+  };
+  const like_count = 3;
+
   return (
     <div style={{ flex: 1, display: 'flex', flexDirection: 'column', gap: 8 }}>
       {noticeList.map((notice) => (
@@ -31,6 +44,18 @@ const NoticePanel = ({ noticeList }: { noticeList: INotice[] }) => {
           {notice.image_url ? (
             <Image src={notice.image_url} alt={notice.title} />
           ) : null}
+          <div className="like-button">
+            <button
+              style={{ background: 'none', border: 'none', cursor: 'pointer' }}
+              onClick={() => {
+                handleLike(notice.id);
+              }}
+            >
+              {isLike[notice.id] ? 'Unlike' : 'Like'}
+            </button>
+            {'Count ' +
+              (isLike[notice.id] ? `${like_count + 1}` : `${like_count}`)}
+          </div>
         </NoticeCard>
       ))}
     </div>
