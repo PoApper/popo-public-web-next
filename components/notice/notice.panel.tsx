6f37b16cae4a3a42d613a5602c922ef98e618b7a
Gwanho Kim <khkim6040@postech.ac.kr>
2024-07-03 22:30:01 +0900

    Change NoticePanel component to handle like functionality and user authentication
    Unfinished

--- components/notice/notice.panel.tsx
+++ components/notice/notice.panel.tsx
@@ -1,20 +1,65 @@
+import { PoPoAxios } from '@/lib/axios.instance';
 import { INotice } from '@/types/notice.interface';
-import { useState } from 'react';
+import { IUser } from '@/types/user.interface';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { Image } from 'semantic-ui-react';
 import styled from 'styled-components';
 
 const NoticePanel = ({ noticeList }: { noticeList: INotice[] }) => {
+  const router = useRouter();
+  const [user, setUser] = useState<IUser | null>({
+    name: '',
+  });
+
+  useEffect(() => {
+    PoPoAxios.get('/auth/verifyToken', {
+      withCredentials: true,
+    })
+      .then((res) => setUser(res.data))
+      .catch(() => setUser(null));
+  }, []);
+
+  const [likes, setLikes] = useState<number[]>(
+    noticeList.map((notice) => notice.like_count),
+  );
   const [isLike, setIsLike] = useState<boolean[]>(
     new Array(noticeList.length).fill(false),
   );
-  const handleLike = (id: number) => {
-    setIsLike((prevIsLike) => {
-      const newLike = [...prevIsLike];
-      newLike[id] = !newLike[id];
-      return newLike;
-    });
+
+  useEffect(() => {
+    const fetchLikeStatus = async () => {
+      // fetch like status
+      const statuses = await Promise.all(
+        noticeList.map((notice) =>
+          PoPoAxios.get(`/notice/${notice.id}/like`, { params: { user } }),
+        ),
+      );
+      setIsLike(statuses.map((status) => status.data));
+    };
+    if (user) fetchLikeStatus();
+  }, [noticeList, user]);
+
+  const handleLike = async (id: number) => {
+    if (!user) {
+      alert('로그인이 필요합니다.');
+      router.push('/auth/login');
+      return;
+    }
+
+    try {
+      // TODO: Call API
+      setIsLike((prevIsLike) => {
+        const newLike = [...prevIsLike];
+        newLike[id] = !newLike[id];
+        return newLike;
+      });
+    } catch (err) {
+      alert('좋아요에 실패했습니다.');
+      console.log(err);
+    }
   };
-  const like_count = 3;
+  const like_count = 3; // 임시로 3으로 설정
 
   return (
     <div style={{ flex: 1, display: 'flex', flexDirection: 'column', gap: 8 }}>
@@ -51,10 +96,11 @@ const NoticePanel = ({ noticeList }: { noticeList: INotice[] }) => {
                 handleLike(notice.id);
               }}
             >
+              {/* TODO: 아이콘 interactive하게 변경 */}
               {isLike[notice.id] ? 'Unlike' : 'Like'}
             </button>
-            {'Count ' +
-              (isLike[notice.id] ? `${like_count + 1}` : `${like_count}`)}
+            {/* TODO: likes로 변경 */}
+            <span style={{ marginLeft: 8 }}>{like_count}</span>{' '}
           </div>
         </NoticeCard>
       ))}
