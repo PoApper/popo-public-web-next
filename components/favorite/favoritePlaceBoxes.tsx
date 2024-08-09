4c9d22e4a72ef003c27c2636d087972ddc6b9bac
HyoJeong Yun <hodori@postech.ac.kr>
2024-08-09 19:51:41 +0900

    log: user_id

--- components/favorite/favoritePlaceBoxes.tsx
+++ components/favorite/favoritePlaceBoxes.tsx
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { PoPoAxios } from '@/lib/axios.instance';
 
 import { IFavoritePlace } from '@/types/favorite.interface';
-import FavoriteBox from './favoritePlaceBox';
+import FavoriteBox from '@/components/favorite/favoritePlaceBox';
 
 const FavoriteBoxes = ({ userId }: { userId: string }) => {
   const [placeIds, setPlaceIds] = useState([]);
   useEffect(() => {
     PoPoAxios.get(`/favorite-place/user_id/${userId}`)
       .then((res) => {
+        console.log(userId);
         setPlaceIds(res.data.map((place: IFavoritePlace) => place.place_id));
       })
       .catch((err) => {
