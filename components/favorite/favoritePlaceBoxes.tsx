5a7f4043552c58f3fe02293a23f43a3c32369437
HyoJeong Yun <hodori@postech.ac.kr>
2024-08-09 22:55:14 +0900

    check: style for fav card

--- components/favorite/favoritePlaceBoxes.tsx
+++ components/favorite/favoritePlaceBoxes.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { PoPoAxios } from '@/lib/axios.instance';
-import { Card, Grid, Image, Button, Icon } from 'semantic-ui-react';
+import { Card, Grid, Image, Button, Icon, ButtonGroup } from 'semantic-ui-react';
 
 import { IPlace } from '@/types/favorite.interface';
 
@@ -17,7 +17,7 @@ const FavoritePlaceBoxes = ({ placeList }: { placeList: IPlace[] }) => {
   };
 
   return (
-    <Grid stackable centered columns={3} style={{ maxWidth: 900 }}>
+    <Grid stackable centered columns={3} style={{ maxWidth: 1000 }}>
       {placeList.map((place: IPlace) => (
         <Grid.Column key={place.uuid}>
           <Card
@@ -28,24 +28,55 @@ const FavoritePlaceBoxes = ({ placeList }: { placeList: IPlace[] }) => {
             <Card.Content>
               <Card.Header>{place.name}</Card.Header>
               <Card.Meta>{place.region}</Card.Meta>
-              <Button
-                icon
-                labelPosition="left"
-                onClick={() =>
-                  handleDelete(`favorite-place/${place.favorite_id}`)
-                }
-              >
-                <Icon name="cancel" />
-                삭제하기
-              </Button>
-              <Button icon labelPosition="right">
-                예약하기
-                <Icon name="arrow right" />
-              </Button>
+              <ButtonGroup>
+                <Button
+                  icon
+                  labelPosition="left"
+                  onClick={() =>
+                    handleDelete(`favorite-place/${place.favorite_id}`)
+                  }
+                >
+                  <Icon name="cancel" />
+                  삭제하기
+                </Button>
+                <Button icon labelPosition="right">
+                  예약하기
+                  <Icon name="arrow right" />
+                </Button>
+              </ButtonGroup>
+            </Card.Content>
+          </Card>
+          <Card
+            href={`/reservation/place/${place.region}/${place.name}`}
+            centered
+          >
+            <Card.Content style={{ display: 'flex', 'align-items': 'center' }}>
+              <div>
+                <Card.Header style={{ color: 'black', 'font-weight': 'bold' }}>
+                  {place.name}
+                </Card.Header>
+              </div>
             </Card.Content>
           </Card>
         </Grid.Column>
-      ))}
+      ))
+      }
+      {placeList.length < 3 && (
+        <Grid.Column>
+          <Card
+            href={`/reservation/place`}
+            centered
+          >
+            <Card.Content style={{ display: 'flex', 'align-items': 'center' }}>
+              <div>
+                <Card.Header style={{ color: 'black', 'font-weight': 'bold' }}>
+                  더 많은 장소 보기
+                </Card.Header>
+              </div>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+      )}
     </Grid>
   );
 };
