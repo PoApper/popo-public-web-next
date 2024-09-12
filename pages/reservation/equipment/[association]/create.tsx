94cdb61ab864138b1d21bdc341cdfd2de5cee6e9
BlueHorn07 <bluehornblues@gmail.com>
2024-09-12 12:35:07 +0900

    setup https

--- pages/reservation/equipment/[association]/create.tsx
+++ pages/reservation/equipment/[association]/create.tsx
@@ -50,7 +50,6 @@ const EquipReservationCreatePage: React.FunctionComponent<{
     PoPoAxios.get('/auth/verifyToken', { withCredentials: true })
       .then((res) => setUserInfo(res.data))
       .catch(() => {
-        if (process.env.NEXT_PUBLIC_ENV === 'local') return;
         alert('로그인 후 예약 할 수 있습니다.');
         router.push('/auth/login');
       });
