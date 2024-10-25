0b008f87106b75e7fff128f7de7219edcfd7d5ff
Gwanho Kim <khkim6040@postech.ac.kr>
2024-10-24 22:18:57 -0700

    Fix hour getting function

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,9 +14,8 @@ const HomePage: React.FunctionComponent<{
   noticeList: INotice[];
   nextEvent: ICalendar;
 }> = ({ noticeList, nextEvent }) => {
-  const isDayTime = 9 <= new Date().getHours() && new Date().getHours() <= 18;
-  console.log(new Date().getHours());
-  console.log(isDayTime);
+  const KRHour = new Date().getUTCHours() + 9;
+  const isDayTime = KRHour >= 6 && KRHour < 18;
 
   return (
     <Layout>
